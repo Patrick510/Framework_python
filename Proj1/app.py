b0792bcbb4d329cc186a6feb5462e5eb093fb9b8
Patrick510 <patrickdedinho321xz@gmail.com>
2024-10-23 20:22:10 -0400

    Add delay between file prints in app.py

--- Proj1/app.py
+++ Proj1/app.py
@@ -1,8 +1,10 @@
 import os
 from tqdm import tqdm
+import time
 
 directory = os.listdir(r"C:\Users\Patrick Dias\Downloads\ESTOQUE")
 
 for file in tqdm(directory):
   print(file)
+  time.sleep(0.1)
 print(directory)
